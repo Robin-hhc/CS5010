8873572a3fa24743bd7bffa3434dc43c98b590ca
Robin-hhc <robinhuangqwe@gmail.com>
2024-02-05 16:04:09 -0500

    test

---
+++ test/src/test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class test {
+  public static void main(String[] args) {
+    String s1 = "ace";
+    String s2 = "abf";
+    System.out.println(s1.compareTo(s2));
+  }
+}
