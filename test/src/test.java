3ad216203161a1df5d453a8000356c076d852d11
huan1323 <robin.huang@mail.utoronto.ca>
2024-02-06 17:32:26 -0500

    mid1

--- test/src/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class test {
-  public static void main(String[] args) {
-    String s1 = "ace";
-    String s2 = "abf";
-    System.out.println(s1.compareTo(s2));
-  }
-}
