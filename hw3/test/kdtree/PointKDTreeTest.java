9914db42d932ec292d7044eb5cdbfc29259ad2db
huan1323 <robin.huang@mail.utoronto.ca>
2024-02-15 14:16:37 -0500

    HW3

--- hw3/test/kdtree/PointKDTreeTest.java
+++ hw3/test/kdtree/PointKDTreeTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * A JUnit test class for the PointKDTree class.
@@ -46,16 +47,45 @@ public class PointKDTreeTest {
     Point2D[] resSorted = res.getPoints().toArray(new Point2D[0]);
     Arrays.sort(resSorted, (a, b) -> Integer.compare(a.x, b.x));
     Assert.assertArrayEquals(expect, resSorted);
+
+    res.add(new Point2D(30, 30));
+    expect = new Point2D[]{new Point2D(0, 0), new Point2D(0, 10), new Point2D(10, 10), new Point2D(10, 10), new Point2D(20, 20), new Point2D(30, 30)};
+    resSorted = res.getPoints().toArray(new Point2D[0]);
+    Arrays.sort(resSorted, (a, b) -> Integer.compare(a.x, b.x));
+    Assert.assertArrayEquals(expect, resSorted);
+
+    res.add(new Point2D(-1, -1));
+    expect = new Point2D[]{new Point2D(-1, -1), new Point2D(0, 0), new Point2D(0, 10), new Point2D(10, 10), new Point2D(10, 10), new Point2D(20, 20), new Point2D(30, 30)};
+    resSorted = res.getPoints().toArray(new Point2D[0]);
+    Arrays.sort(resSorted, (a, b) -> Integer.compare(a.x, b.x));
+    for (Point2D p: resSorted) {
+      System.out.print(p.x);
+      System.out.println(p.y);
+    }
+    Assert.assertArrayEquals(expect, resSorted);
+
+    res.add(new Point2D(0, 0));
+    expect = new Point2D[]{new Point2D(-1, -1), new Point2D(0, 0), new Point2D(0, 0), new Point2D(0, 10), new Point2D(10, 10), new Point2D(10, 10), new Point2D(20, 20), new Point2D(30, 30)};
+    resSorted = res.getPoints().toArray(new Point2D[0]);
+    Arrays.sort(resSorted, (a, b) -> Integer.compare(a.x, b.x));
+    Assert.assertArrayEquals(expect, resSorted);
   }
 
   @Test
   public void allPointsWithinCircleTest() {
     PointKDTree res = new PointKDTree(points);
-//    for (Point2D p: res.allPointsWithinCircle(new Point2D(-1, 10), 11)) {
-//      System.out.print(p.x);
-//      System.out.println(p.y);
-//    }
     Point2D[] expect = new Point2D[]{new Point2D(0, 0), new Point2D(10, 10), new Point2D(10, 10)};
     Assert.assertArrayEquals(expect, res.allPointsWithinCircle(new Point2D(-1, 10), 11).toArray(new Point2D[0]));
+    expect = new Point2D[]{new Point2D(10, 10), new Point2D(10, 10)};
+    Assert.assertArrayEquals(expect, res.allPointsWithinCircle(new Point2D(4, 10), 7).toArray(new Point2D[0]));
+
+  }
+
+  @Test
+  public void closestPointTest() {
+    PointKDTree res = new PointKDTree(points);
+    assertEquals(this.points[0], res.closestPoint(new Point2D(1, 1)));
+    assertEquals(this.points[1], res.closestPoint(new Point2D(11, 11)));
+    assertEquals(this.points[2], res.closestPoint(new Point2D(15, 15)));
   }
 }
