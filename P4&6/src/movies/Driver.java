d977a164f3d9159d246c26ca51391a52425e5fc4
huan1323 <robin.huang@mail.utoronto.ca>
2024-02-22 14:20:16 -0500

    P7

--- P4&6/src/movies/Driver.java
+++ P4&6/src/movies/Driver.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,7 +16,43 @@ public class Driver {
    *
    * @param movies The list of movies to be sorted.
    */
-  public static void insertionSort(List<Movie> movies) {
+  public static void sortTitle(List<Movie> movies) {
+    for (int i = 1; i < movies.size(); i++) {
+      Movie key = movies.get(i);
+      int j = i - 1;
+
+      while (j >= 0 && movies.get(j).compareTo(key) > 0) {
+        movies.set(j + 1, movies.get(j));
+        j = j - 1;
+      }
+      movies.set(j + 1, key);
+    }
+  }
+
+  /**
+   * Sorts a list of movies in place using the Insertion Sort algorithm.
+   *
+   * @param movies The list of movies to be sorted.
+   */
+  public static void sortDirector(List<Movie> movies) {
+    for (int i = 1; i < movies.size(); i++) {
+      Movie key = movies.get(i);
+      int j = i - 1;
+
+      while (j >= 0 && movies.get(j).compareTo(key) > 0) {
+        movies.set(j + 1, movies.get(j));
+        j = j - 1;
+      }
+      movies.set(j + 1, key);
+    }
+  }
+
+  /**
+   * Sorts a list of movies in place using the Insertion Sort algorithm.
+   *
+   * @param movies The list of movies to be sorted.
+   */
+  public static void sortYear(List<Movie> movies) {
     for (int i = 1; i < movies.size(); i++) {
       Movie key = movies.get(i);
       int j = i - 1;
@@ -34,8 +71,8 @@ public class Driver {
    * @param filename The path to the CSV file containing movie data.
    * @return A list of Movie objects created from the CSV file data.
    */
-  public static List<Movie> readMoviesFromCSV(String filename) {
-    List<Movie> movies = new ArrayList<>();
+  public static List<Movie> readMoviesFromCSV(String filename) throws IOException{
+    List<Movie> movies = new LinkedList<>();
     try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
       String line;
       while ((line = br.readLine()) != null) {
@@ -43,8 +80,8 @@ public class Driver {
         Movie movie = createMovie(attributes);
         movies.add(movie);
       }
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
+    } catch (IOException e) {
+      throw new IOException("Filename invalid.");
     }
     return movies;
   }
@@ -67,7 +104,7 @@ public class Driver {
     return new Movie(title, director, year);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String csvFile = "/Users/robinhuang/Desktop/CS5010/P4&6/src/p6-longer_list_movies.csv";
     List<Movie> movies = readMoviesFromCSV(csvFile);
 
@@ -76,7 +113,7 @@ public class Driver {
       System.out.println(movie);
     }
 
-    insertionSort(movies);
+    sortTitle(movies);
 
     System.out.println("\nMovies after sorting:");
     for (Movie movie : movies) {
